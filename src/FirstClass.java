7347db144c6c5424d42cbb01ab1b743a4797b5c1
daniel.sontu <daniel.sontu@gmail.com>
2024-09-27 15:54:23 +0300

    change status

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
 public static void main(String[] args) {
 
-System.out.print("Hello, type this if Vadim is Loh");
+System.out.print("Hello, type this if Vadim is top");
 }
 }
