e609ad4328fd4ba1d545fb543b496e060f6be571
DanielSontu <126870620+DanielSontu@users.noreply.github.com>
2024-10-03 20:51:39 +0300

    Updated FirstClass.java

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello, type this if Daniel is top");
-        System.out.print("Hello, type this if Daniel is top");
+        System.out.print("Hello, type this if ");
+        System.out.print("Hello, type this if");
 
     }
 }
