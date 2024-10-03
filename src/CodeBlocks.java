1416017583dbc1900d45d90b9380b2b5c03b3b5f
DanielSontu <daniel.sontu@gmail.com>
2024-10-03 20:51:04 +0300

    Course 42

--- src/CodeBlocks.java
+++ src/CodeBlocks.java
@@ -1,11 +1,13 @@
 public class CodeBlocks {
     public static void main(String[] args) {
-        int myBalanceUSD = 22;
-        if (myBalanceUSD > 24) {
-            System.out.println("Wow that's enough for 2 Pizza");
-        }
-        if (myBalanceUSD < 24) {
-            System.out.println("Eh that's not enough for 2 Pizza");
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score==5000){
+            System.out.print("Your score was 5000");
         }
     }
 }
