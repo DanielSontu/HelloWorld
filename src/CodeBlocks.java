022930b5a8c2d0f27d50765314e35395f22a5dd9
DanielSontu <daniel.sontu@gmail.com>
2024-10-03 20:38:15 +0300

    Course 42

---
+++ src/CodeBlocks.java
@@ -0,0 +1,11 @@
+public class CodeBlocks {
+    public static void main(String[] args) {
+        int myBalanceUSD = 22;
+        if (myBalanceUSD > 24) {
+            System.out.println("Wow that's enough for 2 Pizza");
+        }
+        if (myBalanceUSD < 24) {
+            System.out.println("Eh that's not enough for 2 Pizza");
+        }
+    }
+}
