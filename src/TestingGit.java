ee06308cf06e0bc9298c3df11520e423655b8ad8
daniel.sontu <daniel.sontu@gmail.com>
2024-10-04 17:10:28 +0300

    added TestingGit

---
+++ src/TestingGit.java
@@ -0,0 +1,6 @@
+public class TestingGit {
+    public static void main(String[] args) {
+        System.out.print("Hello, type this if Daniel is top");
+        System.out.print("Hello, type this if Daniel is top");
+    }
+}
