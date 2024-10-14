73b252ce97c732e2e75f47b15e96ffddbdcf03e9
DanielSontu <daniel.sontu@gmail.com>
2024-10-14 12:12:41 +0300

    Course 50 CodingExercise

---
+++ src/CodingExercise1.java
@@ -0,0 +1,12 @@
+public class CodingExercise1 {
+
+    public static void checkNumber(int number) {
+        if (number>0){
+            System.out.print("positive");
+        } else if(number<0){
+            System.out.print("negative");
+        } else
+            System.out.println("zero");
+        }
+    }
+
