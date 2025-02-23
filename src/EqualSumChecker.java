21dcb0f9749d2303338fc12e35ab2651cd8afbca
DanielSontu <daniel.sontu@gmail.com>
2025-02-23 21:50:49 +0200

    CodingExercise 7

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int Num1, int Num2, int Num3){
+
+        if (Num1+Num2==Num3)
+            return true;
+        else
+            return false;
+    }
+}
