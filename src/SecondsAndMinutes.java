c68c97bea4f71c2af82249679f7e8c7e5701edbe
DanielSontu <daniel.sontu@gmail.com>
2025-03-02 19:50:34 +0200

    bonus challenge

--- src/SecondsAndMinutes.java
+++ src/SecondsAndMinutes.java
@@ -2,23 +2,37 @@ public class SecondsAndMinutes {
 
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(3945)); //first test-case
-        System.out.println(getDurationString(65,45)); //second test-case
+        System.out.println(getDurationString(-3945)); //first test-case
+        System.out.println(getDurationString(-65, 45)); //second test-case
+        System.out.println(getDurationString(65, 145));
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
     }
-        public static String getDurationString(int seconds){
-        //Two step approach to get hours
-        int minutes = seconds/60;
-        return getDurationString(minutes, seconds);
+
+    public static String getDurationString(int seconds) {
+
+        if (seconds < 0) {
+            return "Invalid data for seconds(" + seconds + "), must be a positive integer value";
         }
 
-        public static String getDurationString(int minutes, int seconds){
-
-            int hours = minutes/60;
-
-            int remainingMinutes = minutes % 60;
-            int remainingSeconds = seconds % 60;
-
-            return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s ";
-        }
+        int minutes = seconds / 60;
+        return getDurationString(seconds / 60, seconds % 60);
     }
+
+    public static String getDurationString(int minutes, int seconds) {
+
+        if (minutes < 0) {
+            return "Invalid data for minutes(" + minutes + "), must be a positive integer value";
+        }
+
+        if (seconds < 0 || seconds > 59) {
+            return "Invalid data for seconds(" + seconds + ") must be a value between 0 and 59";
+        }
+        int hours = minutes / 60;
+
+        int remainingMinutes = minutes % 60;
+
+        return hours + "h " + remainingMinutes + "m " + seconds + "s ";
+    }
+}
 
