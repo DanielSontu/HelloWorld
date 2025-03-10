8377f65b82bdbf59a90dcc8732e074665ac74950
DanielSontu <daniel.sontu@gmail.com>
2025-03-10 22:31:07 +0200

    Enhanced Switch

--- src/SwitchStatements.java
+++ src/SwitchStatements.java
@@ -9,8 +9,10 @@ public class SwitchStatements {
                 break;
             case 2:
                 System.out.print("Value was 2");
-               break;
-            case 3: case 4: case 5:
+                break;
+            case 3:
+            case 4:
+            case 5:
                 System.out.println("Value was a 3, a 4, or a 5 ");
                 System.out.println("Actually it was a " + switchValue);
                 break;
@@ -18,19 +20,36 @@ public class SwitchStatements {
                 System.out.print("Was not 1, 2, 3, 4 or 5");
                 break;
         }
+        String month = "JANUARY";
+        System.out.println(month + " is in the " + getQuarter(month)+ " quarter");
     }
-}
 
+    public static String getQuarter(String month) {
 
-  /* int value = 1;
-            if (value == 1){
-            System.out.print("Value was 1");
-        } else if (value ==2) {
-            System.out.print("Value was 2");
-            }else {
-            System.out.print("Value was not 1 or 2");
-
-        }
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> { yield "1st";}
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                String badResponse = month + " is bad ";
+                yield badResponse;
+            }
+            };
+        };
     }
-}
-*/
+
+
+
+
+//   int value = 1;
+//            if (value == 1){
+//            System.out.print("Value was 1");
+//        } else if (value ==2) {
+//            System.out.print("Value was 2");
+//            }else {
+//            System.out.print("Value was not 1 or 2");
+//
+//        }
+//    }
+//}
