0046c81c1d36809680ea21a181fa127c6e01a464
daniel.sontu <daniel.sontu@gmail.com>
2025-03-12 23:05:47 +0200

    Traditional Switch Challenge

---
+++ src/SwitchClass.java
@@ -0,0 +1,26 @@
+public class SwitchClass {
+    public static void main(String[] args) {
+
+        char phoneticAlpha = 'A';
+
+        switch (phoneticAlpha) {
+            case 'A':
+                System.out.print("A is Able");
+                break;
+            case 'B':
+                System.out.println("B is Baker");
+                break;
+            case 'C':
+                System.out.println("C is Charlie");
+                break;
+            case 'D':
+                System.out.println("D is Dog");
+                break;
+            case 'E':
+                System.out.println("E is Easy");
+                break;
+            default:
+                System.out.print("Letter " + phoneticAlpha + " was not Found ");
+        }
+    }
+}
