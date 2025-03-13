f4823fd4cacd927f094e8d42365aca3fafdece3f
daniel.sontu <daniel.sontu@gmail.com>
2025-03-13 14:26:27 +0200

    Improved Code

--- src/SwitchStatements.java
+++ src/SwitchStatements.java
@@ -1,31 +1,42 @@
+import java.util.Scanner;
+
 public class SwitchStatements {
+
     public static void main(String[] args) {
+        String test = "";
+        while (!test.equals("n")) {
+            System.out.print("Please input a number between 1 and 6: ");
+            Scanner keyboard = new Scanner(System.in);
+            int switchValue = keyboard.nextInt();
+            System.out.print("Please input a month: ");
 
-        int switchValue = 3;
+            String month = keyboard.next();
 
-        switch (switchValue) {
-            case 1:
-                System.out.print("Value was 7");
-                break;
-            case 2:
-                System.out.print("Value was 2");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Value was a 3, a 4, or a 5 ");
-                System.out.println("Actually it was a " + switchValue);
-                break;
-            default:
-                System.out.print("Was not 1, 2, 3, 4 or 5");
-                break;
+            switch (switchValue) {
+                case 1:
+                    System.out.println("Value was 7");
+                    break;
+                case 2:
+                    System.out.println("Value was 2");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Value was a 3, a 4, or a 5 ");
+                    System.out.println("Actually it was a " + switchValue);
+                    break;
+                default:
+                    System.out.println("Was not 1, 2, 3, 4 or 5");
+                    break;
+            }
+
+            System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+            System.out.print("Continue y/n = ");
+            test = keyboard.next();
         }
-        String month = "JANUARY";
-        System.out.println(month + " is in the " + getQuarter(month)+ " quarter");
     }
 
     public static String getQuarter(String month) {
-
         return switch (month) {
             case "JANUARY", "FEBRUARY", "MARCH" -> { yield "1st";}
             case "APRIL", "MAY", "JUNE" -> "2nd";
@@ -36,6 +47,7 @@ public class SwitchStatements {
                 yield badResponse;
             }
             };
+
         };
     }
 
