372c4b39cae437a3d2b9afdf381cc6852be695d3
daniel.sontu <daniel.sontu@gmail.com>
2025-03-19 19:28:08 +0200

    Solved challenge about sales with promocodes

---
+++ src/SalesChallenge.java
@@ -0,0 +1,18 @@
+public class SalesChallenge {
+    public static void main(String[] args) {
+       int updatedPrice = productPrice(700, "SPRING20");
+    System.out.println("Your price is " + updatedPrice  );
+    }
+
+    public static int productPrice(int price, String promocode) {
+        int updatedPrice = price;
+        switch (promocode) {
+            case "WELCOME10" -> updatedPrice = price - (price * 10 / 100);
+            case "SPRING20" -> updatedPrice = price - (price * 20 / 100);
+            case "VIP30" -> updatedPrice = price - (price * 30 / 100);
+            default -> System.out.println("No sales ");
+        }
+        return updatedPrice;
+    }
+
+}
