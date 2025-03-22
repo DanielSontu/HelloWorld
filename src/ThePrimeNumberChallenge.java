09377c5280616ce94c005743c3992d3438a92a22
DanielSontu <daniel.sontu@gmail.com>
2025-03-22 11:39:53 +0200

    Prime numbers challenge

--- src/ThePrimeNumberChallenge.java
+++ src/ThePrimeNumberChallenge.java
@@ -1,29 +1,30 @@
 public class ThePrimeNumberChallenge {
     public static void main(String[] args) {
-
-
-        System.out.println("0 is " + (isPrime(0) ? "" : "NOT ") + " a prime number");
-        System.out.println("1 is " + (isPrime(1) ? "" : "NOT ") + " a prime number");
-        System.out.println("2 is " + (isPrime(2) ? "" : "NOT ") + " a prime number");
-        System.out.println("3 is " + (isPrime(3) ? "" : "NOT ") + " a prime number");
-
-        System.out.println("8 is " + (isPrime(8) ? "" : "NOT ") + " a prime number");
-        System.out.println("17 is " + (isPrime(13) ? "" : "NOT ") + " a prime number");
-        System.out.println("99 is " + (isPrime(13) ? "" : "NOT ") + " a prime number");
-
-
+        int count = 0;
+        for (int i = 10; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println("number " + i + " is a Prime number");
+                count++;
+            } else if (count >= 3) {
+                System.out.println("Found 3, exiting the loop");
+                break;
+            }
+        }
     }
 
     public static boolean isPrime(int wholeNumber) {
 
-        if (wholeNumber <= 2){
-            return (wholeNumber==2);
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
         }
         for (int divisor = 2; divisor <= wholeNumber / 2; divisor++) {
-        if (wholeNumber % divisor==0){
-            return false;
-        }
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
         }
         return true;
     }
 }
+//This code checks for prime numbers between 10 and 50, and prints the first 3 ones.
+
+
